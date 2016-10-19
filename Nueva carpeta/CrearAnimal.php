8f383f10faa8aed0ed5c86b71c6c45b2ed23825a
guille777 <gbrfloridaa@gmail.com>
2016-10-19 12:37:15 +0200

    animal

---
+++ Nueva carpeta/CrearAnimal.php
@@ -0,0 +1,29 @@
+<?php 
+include "animal.php";
+//geters:
+$animal1= new animal();
+$tipo=$animal1->gettipo();
+echo $tipo. "br";
+
+$patas=$animal1->getpatas();
+echo $patas. "br";
+//
+
+$animal2-<settipo("cienpies");
+$animal2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
